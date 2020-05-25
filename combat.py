b375a7f6ae9efd85c234e76de6d7c18fbe600e91
Hectoralvf <60094746+Hectoralvf@users.noreply.github.com>
2020-05-25 18:44:01 +0200

    created move and pokemon classes

--- combat.py
+++ combat.py
@@ -2,9 +2,118 @@ import pygame
 import json
 
 
+class Move():
+    def __init__(self, move_id): 
+        with open("data/movedex.json") as f_movedex:
+            dic_movedex = json.load(f_movedex)
+            self.moveID = move_id
+            self.name = dic_movedex[move_id]['name']
+            self.type = dic_movedex[move_id]['type']
+            self.category = dic_movedex[move_id]['category']
+            self.turns = dic_movedex[move_id]['turns']
+            self.power = dic_movedex[move_id]['power']
+            self.accuracy = dic_movedex[move_id]['accuracy']
+            self.priority = dic_movedex[move_id]['priority']
+            self.pps = dic_movedex[move_id]['pps']
+            self.stats_user = dic_movedex[move_id]['stats_user']
+            self.stats_foe = dic_movedex[move_id]['stats_foe']
+            self.status_user = dic_movedex[move_id]['status_user']
+            self.status_foe = dic_movedex[move_id]['status_foe']
+            self.hp_user = dic_movedex[move_id]['hp_user']
+            self.hp_foe = dic_movedex[move_id]['hp_foe']
+    
+    def spendPP(self):
+        self.pps -= 1
+    
+    def __str__(self):
+        return """Move(
+    '{}', 
+    '{}', 
+    '{}', 
+    '{}', 
+    '{}', 
+    '{}', 
+    '{}', 
+    '{}', 
+    '{}', 
+    '{}', 
+    '{}', 
+    '{}',
+    '{}', 
+    '{}', 
+    '{}'
+)""".format(self.moveID, self.name, self.type, self.category, self.turns, self.power,self.accuracy, self.priority, self.pps, self.stats_user, self.stats_foe, self.status_user, self.status_foe, self.hp_user, self.hp_foe)
+
+class Pokemon():
+    def __init__(self, pid):
+        with open("data/pokedex.json") as f_pokedex:
+            dic_pokedex = json.load(f_pokedex)
+            self.p_id = dic_pokedex[pid]['pid']
+            self.ko = False
+            self.name = dic_pokedex[pid]['name']
+            self.main_type = dic_pokedex[pid]['mainType']
+            self.sec_type = dic_pokedex[pid]['secType']
+            self.moveset = []
+            self.moveset.append(Move(dic_pokedex[pid]['moveset'][0]))
+            self.moveset.append(Move(dic_pokedex[pid]['moveset'][1]))
+            self.moveset.append(Move(dic_pokedex[pid]['moveset'][2]))
+            self.moveset.append(Move(dic_pokedex[pid]['moveset'][3]))
+            self.HP = ((dic_pokedex[pid]['baseHP']*2 + 31 + 21)+110)  #omitted level * and / because every pokemon is lvl100, and 100/100=0
+            self.ATT = ((dic_pokedex[pid]['baseAtt']*2 + 31 + 21) + 5)  #omitted level because every pokemon is lvl100 and omited nature for the sake of simplicity
+            self.DEF_ = ((dic_pokedex[pid]['baseDef']*2 + 31 + 21) + 5)  #omitted level because every pokemon is lvl100 and omited nature for the sake of simplicity
+            self.SP_ATT = ((dic_pokedex[pid]['baseSpAtt']*2 + 31 + 21) + 5)  #omitted level because every pokemon is lvl100 and omited nature for the sake of simplicity
+            self.SP_DEF = ((dic_pokedex[pid]['baseSpDef']*2 + 31 + 21) + 5)  #omitted level because every pokemon is lvl100 and omited nature for the sake of simplicity
+            self.SPEED = ((dic_pokedex[pid]['baseSpe']*2 + 31 + 21) + 5)  #omitted level because every pokemon is lvl100 and omited nature for the sake of simplicity
+            self.current_hp = self.HP
+            self.current_att = self.ATT
+            self.current_def = self.DEF_
+            self.current_sp_att = self.SP_ATT
+            self.current_sp_def = self.SP_DEF
+            self.current_speed = self.SPEED
+            self.stage_att = 0
+            self.stage_def = 0
+            self.stage_sp_att = 0
+            self.stage_sp_def = 0
+            self.stage_speed = 0
+            self.accuracy = 1.0
+            self.evasion = 1.0
+            self.stage_accuracy = 0
+            self.stage_evasion = 0
+            self.status_list = []
+
+    def __str__(self):
+        return """#{}: {}
+KO: {}
+{} / {}
+
+HP: {}/{}
+Att: {}/{}  {}
+Def: {}/{}  {}
+Sp Att: {}/{}  {}
+Sp Def: {}/{}  {}
+Speed: {}/{}  {}
+
+Accu: {}  {}
+Eva: {}  {}
+
+States: {}
+
+Moves: {}
+""".format(self.p_id, self.name, self.ko, self.main_type, self.sec_type, self.current_hp, self.HP, self.current_att, self.ATT, self.stage_att, self.current_def, self.DEF_, self.stage_def, self.current_sp_att, self.SP_ATT, self.stage_sp_att, self.current_sp_def, self.SP_DEF, self.stage_sp_def, self.current_speed, self.SPEED, self.stage_speed, self.accuracy, self.stage_accuracy, self.evasion, self.stage_evasion, self.status_list, self.moveset)
+
+
 endCombat = False
 
 def buildTeams():
+    pass
+
+test = Pokemon('3')
+
+print(test.__str__())
+# print(test.moveset[0].__str__())
+# test.moveset[0].spendPP()
+# print(test.moveset[0].__str__())
+
 
 
 while endCombat != True: 
